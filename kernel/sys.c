7bce1e8a6c0ebc1e0fb4484ac8f535d89cfb482a
AnymoreProject <anymore.dev13@gmail.com>
2025-07-17 16:38:14 +0000

    [BACKPORT] fs: implement SUSFS v1.5.9 There is a few change in here: - sus_mount functionality still remain in v1.5.5 as backporting it to the latest version will result a mount detection leak in some apps/detectors
    Thank you @simonpunk for the help of sus_path backport
    
    Co-authored-by: sidex15 <24408329+sidex15@users.noreply.github.com>
    Co-authored-by: simonpunk <simonpunk2016@gmail.com>
    Signed-off-by: @AnymoreProject <anymore.dev13@gmail.com>
    (cherry picked from commit a1aa34681e1fa801b702f4a036bf967d5ca367e0)

--- kernel/sys.c
+++ kernel/sys.c
@@ -1185,15 +1185,26 @@ static int override_release(char __user *release, size_t len)
 	return ret;
 }
 
+#ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
+extern void susfs_spoof_uname(struct new_utsname* tmp);
+#endif
+static uint64_t netbpfload_pid = 0;
 SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 {
 	struct new_utsname tmp;
 
 	down_read(&uts_sem);
 	memcpy(&tmp, utsname(), sizeof(tmp));
+#ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
+	if (!strncmp(current->comm, "netbpfload", 10) &&
+	    current->pid != netbpfload_pid) {
+		netbpfload_pid = current->pid;
+		strcpy(tmp.release, "6.6.40");
+		pr_debug("fake uname: %s/%d release=%s\n",
+			 current->comm, current->pid, tmp.release);
+	}
 	up_read(&uts_sem);
 	if (copy_to_user(name, &tmp, sizeof(tmp)))
-		return -EFAULT;
 
 	if (override_release(name->release, sizeof(name->release)))
 		return -EFAULT;
